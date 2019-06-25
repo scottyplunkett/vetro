9dc2ec59613ba38f8513ceae868f4ccc5dcb1b31
Scott Plunkett <splunkett@8thLight.com>
2019-06-24 23:15:57 -0500

    [CONFIG] Setting up to push to Heroku

--- dbconnect.js
+++ dbconnect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 
-const url = "mongodb://localhost:27017/chat";
+const url = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/chat');
 
 const connect = mongoose.connect(url, { useNewUrlParser: true });
 
