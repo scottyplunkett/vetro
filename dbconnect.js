06d1d7b03c7835b5886b66f401ce6f48e5db33f5
Scott Plunkett <splunkett@8thLight.com>
2019-06-24 23:58:48 -0500

    [FIX] Change chat endpoint for fetching.

--- dbconnect.js
+++ dbconnect.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://localhost:27017/chat';
 
-const connect = mongoose.connect(`${process.env.MONGODB_URI || url}/chat`, { useNewUrlParser: true });
+const connect = mongoose.connect(`${process.env.MONGODB_URI || url}`, { useNewUrlParser: true });
 
 module.exports = connect;
